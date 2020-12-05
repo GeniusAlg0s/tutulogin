fad6a44bb0f59eded3163e413653d23d70f22efd
Patrick Williams <51858277+GeniusAlg0s@users.noreply.github.com>
2020-12-05 06:29:31 -0800

    Add files via upload

---
+++ tutu/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php include 'connection.php';?>
+       <form>
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname"><br>
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="lname">
+</form>
+        
+    </body>
+</html>
